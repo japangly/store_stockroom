6c27e02a7737fcfb395747f02cbcee5c80a33b15
hourlee99 <oukchannlyhour@gmail.com>
2019-07-25 18:09:52 +0700

    login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:store_stockroom/home_screen.dart';
+import 'package:store_stockroom/splash_screen.dart';
 
 void main() {
   runApp(
     MaterialApp(
       title: 'store_stockroom',
-      home: HomeScreen(),
+      home: SplashScreen(),
     ),
   );
 }
