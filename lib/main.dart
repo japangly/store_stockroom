01469889b30046b56d75c35d5ff3faa194d00878
hourlee99 <oukchannlyhour@gmail.com>
2019-07-29 17:22:12 +0700

    change password

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:store_stockroom/successful_change.dart';
+import 'package:store_stockroom/home_screen.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'store_stockroom',
-      home: SuccessChangedScreen(),
+      home: HomeScreen(),
     ),
   );
 }
