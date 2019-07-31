4298503bfb6bd6aa042d355200936382ac4bf44d
Japang-LY <japangly@gmail.com>
2019-07-31 20:20:21 +0700

    DateFormat

--- lib/dialogs/print_dialog.dart
+++ lib/dialogs/print_dialog.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/ionicons.dart';
-import 'package:intl/intl.dart';
 import 'package:pdf/pdf.dart';
 import 'package:printing/printing.dart';
 import 'package:recase/recase.dart';
